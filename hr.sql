15d203f14438bc00e5ad660e189347740745a018
HyunWoo <mogs223@gmail.com>
2025-03-18 17:58:42 +0900

    SELECT문 연습

---
+++ hr.sql
@@ -0,0 +1,123 @@
+-- hr.employees = bigger scott emp
+SELECT * FROM hr.EMPLOYEES e;
+
+-- employees - first_name, last_name, job_id를 조회
+SELECT e.FIRST_NAME, e.LAST_NAME, e.JOB_ID FROM EMPLOYEES e;
+
+--Job_id 중복제외 후 조회
+SELECT DISTINCT e.JOB_ID FROM EMPLOYEES e;
+
+-- 사번이 176인 사원의 last_name, 부서 번호 조회
+SELECT
+	e.LAST_NAME,
+	e.DEPARTMENT_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.EMPLOYEE_ID = 176;
+
+-- 급여가 12000 이상 되는 사원들의 last_name과 salary 를 조회
+SELECT
+	e.LAST_NAME,
+	e.SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	e.SALARY > 12000 ;
+
+-- 급여가 5000~12000의 범위가 아닌 사원의 last_name과 salary 를 조회
+SELECT
+	e.LAST_NAME,
+	e.SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	NOT (e.SALARY >= 5000 AND e.SALARY <= 12000);
+
+-- 급여가 5000~12000의 범위가 아닌 사원의 last_name과 salary 를 조회 (BETWEEN 사용)
+SELECT
+	e.LAST_NAME,
+	e.SALARY
+FROM
+	EMPLOYEES e
+WHERE
+	E.SALARY NOT BETWEEN 5000 AND 12000;
+
+-- IN 사용하여 20, 50번 부서에 근무하는 사월 조회(이름, 부서번호) 이름을 오름차순으로 정렬
+SELECT
+	e.LAST_NAME,
+	e.DEPARTMENT_ID
+FROM
+	EMPLOYEES e
+WHERE
+	e.DEPARTMENT_ID IN (20, 50)
+ORDER BY
+	e.LAST_NAME ASC; 
+
+-- 급여가 2500,3500,7000이 아니고 직무가 SA_REP, ST_CLERK가 아닌 사원 조회
+SELECT
+	*
+FROM
+	EMPLOYEES e
+WHERE
+	e.SALARY NOT IN (2500, 3500, 7000)
+	AND e.JOB_ID NOT IN ('SA_REP', 'ST_CLERK');
+
+-- 고용일이 2014년 사원 조회
+SELECT
+	*
+FROM
+	EMPLOYEES e
+WHERE
+	e.HIRE_DATE >= '2014-01-01'
+	AND e.HIRE_DATE <= '2014-12-31';
+SELECT
+	*
+FROM
+	EMPLOYEES e
+WHERE
+	e.HIRE_DATE BETWEEN '2014-01-01' AND '2014-12-31';
+
+-- LIKE
+-- 이름에 u가 포함되는 사원들의 사번, 이름을 조회
+SELECT e.EMPLOYEE_ID, e.LAST_NAME
+FROM EMPLOYEES e
+WHERE e.LAST_NAME LIKE '%u%'; 
+
+-- 이름의 4번째 글자가 a인 사원들의 사번, 이름을 조회
+SELECT e.EMPLOYEE_ID, e.LAST_NAME
+FROM EMPLOYEES e
+WHERE e.LAST_NAME LIKE '___a%'; 
+-- 이름에 a 혹은 e 글자가 있는 사원들의 사번, 이름을 조회, 이름을 내림차순으로 정렬
+SELECT e.EMPLOYEE_ID, e.LAST_NAME
+FROM EMPLOYEES e
+WHERE e.LAST_NAME LIKE '%a%' OR e.LAST_NAME LIKE  '%e%'
+--WHERE REGEXP_LIKE(e.LAST_NAME, 'a|e')
+ORDER BY e.LAST_NAME DESC; 
+
+-- 이름에 a와 e가 모두 있는 사원들의 사번, 이름을 조회, 이름을 내림차순으로 정렬
+SELECT e.EMPLOYEE_ID, e.LAST_NAME
+FROM EMPLOYEES e
+WHERE e.LAST_NAME  LIKE '%a%' AND e.LAST_NAME LIKE '%e%' -- = %e%a% OR %a%e%
+--WHERE e.LAST_NAME  LIKE IN ('%a%', '%e%')
+ORDER BY e.LAST_NAME DESC; 
+
+-- COMMISSION_PCT 가 NULL값인 직원을 조회
+SELECT * FROM EMPLOYEES e WHERE e.COMMISSION_PCT IS NULL;
+
+-- MANAGER_ID가 없는 사원들의 LAST_NAME, JOB_ID를 조회
+SELECT e.LAST_NAME, e.JOB_ID FROM EMPLOYEES e WHERE e.MANAGER_ID IS NULL;
+
+-- ST)CLERK인 직업을 가진 사원이 없는 부서 번호 조회(단, 부서번호가 널 값인 부서 제외)
+SELECT DISTINCT
+	e.DEPARTMENT_ID
+FROM
+	EMPLOYEES e
+WHERE
+	NOT e.JOB_ID = 'CLERK'
+	AND e.DEPARTMENT_ID IS NOT NULL;
+-- COMMISSION_PCT가 NULL이 아닌 사원들 중에서 COMMISSION=SALARY * COMMISSION_PCT를 구한다
+-- 계산결과와 함께 사번, FIRST_NAME,JOB_ID를 조회
+SELECT e.EMPLOYEE_ID, e.FIRST_NAME, e.JOB_ID, e.SALARY * e.COMMISSION_PCT AS "COMMISSION" 
+FROM EMPLOYEES e
+WHERE e.COMMISSION_PCT IS NOT NULL ;
--- scott.sql
+++ scott.sql
@@ -47,3 +47,191 @@ SELECT e.DEPTNO FROM EMP e; -- e : 별칭, 있는게 편함
 SELECT deptno FROM emp;
 
 SELECT e.EMPNO, e.DEPTNO FROM emp e; -- 사원번호, 부서번호 조회
+
+-- 중복 데이터 제외하고 조회 (DISTINCT)
+SELECT DISTINCT deptno FROM emp;
+SELECT DISTINCT job, deptno FROM emp; -- job, deptno 둘 다 중복되는것만 제외
+
+-- 연산 후 조회(+ 별칭 짓시)
+SELECT ename, sal, (sal*12 + comm) AS annsal, comm FROM EMP ; -- AS 생략가능
+SELECT ename "사 원 명", sal "급 여", (sal*12 + comm) "aNnSaL", comm FROM EMP ; -- "" 강조가능 (쌍따옴표사용시 무조건 대문자 X, 공백 포함 가능)
+
+-- 원하는 순서대로 출력 데이터를 정렬
+SELECT e.*  FROM EMP e ORDER BY e.sal ASC; -- emp 테이블의 모든 열을 급여 기준(ORDER BY)으로 오름차순(ASC) 조회
+SELECT e.*  FROM EMP e ORDER BY e.sal ASC; -- 오름차순 ASC는 생략 가능
+SELECT e.*  FROM EMP e ORDER BY e.sal DESC; -- emp 테이블의 모든 열을 급여 기준(ORDER BY)으로 내림차순(DESC) 조회
+
+-- 사번, 이름, 직무만 급여 기준으로 내림차순 조회
+SELECT e.EMPNO, e.ENAME, e.JOB FROM EMP e ORDER BY e.sal DESC;
+
+-- 정렬 기준 복수 지정 (부서번호의 오름차순 정렬 후 급여의 내림차순 정렬)
+SELECT e.* FROM EMP e ORDER BY e.deptno ASC, e.sal DESC; -- 정렬 순서에 영향을 끼침
+
+-- 실습 각 열의 별칭을 바꿔서 조회, 부서번호를 내림차순으로 정렬 후 이름을 오름차순으로 
+SELECT // ctrl+f 로 정리가능
+	e.EMPNO "EMPLOYEE_NO",
+	e.ENAME "EMPLOYEE_NAME",
+	e.MGR "MANAGER",
+	e.SAL "SALARY",
+	e.COMM "COMMISSION",
+	e.DEPTNO "DEPARTMENT_NO"
+FROM
+	EMP e
+ORDER BY
+	e.DEPTNO DESC,
+	e.ENAME ASC;
+
+-- 필요한 데이터만 출력 (WHERE : 조회 시 조건 부여)
+-- 부서번호가 30번인 사원들을 조회
+SELECT
+	e.ENAME,
+	e.DEPTNO
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 30;
+
+-- 사번이 7782인 사원 조회
+SELECT
+	*
+FROM
+	emp e
+WHERE
+	e.empno = 7782;
+
+-- 부서번호가 30, 직책이 salesman 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.DEPTNO = 30 AND e.JOB = 'SALESMAN'; -- 오라클에서 문자는 ''만 가능 / 문자는 대소문자 구분함
+	
+-- 사원번호 7499 이고 부서번호가 30인 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.EMPNO = 7499
+	AND e.DEPTNO = 30;
+
+-- 사원번호 7499 이거나 부서번호가 30인 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.EMPNO = 7499
+	OR e.DEPTNO = 30;
+
+-- WHERE 에서도 쓸 수 있는 산술 연산자
+-- 1. 산술연산자 +, -, *, /
+-- 2. 비교연산자 >, <, >=, <=
+-- 3. 등가비교연산자 =, 같지않다 (!= or <> or ^=)
+-- 4. 논리부정연산자 NOT
+-- 5. 			 IN
+-- 6. 범위 연산자 BETWEEN A AND B
+-- 7. 검색 LIKE 연산자와 와일드카드(_, %)
+-- 8. IS NULL NULL과 같다
+-- 연봉이 36000인 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.SAL * 12 = 36000;
+
+-- 급여가 3000 이상인 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.sal > 3000;
+
+-- 급여가 2500 이상, 직원이 ANALYST인 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.sal >= 2500 AND e.job = 'ANALYST';
+
+-- 문자 대소비교
+
+-- 급여가 3000이 아닌 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.sal ^= 3000; -- ^= != <>, NOT e.sal = 3000
+	
+-- 사원이름의 시작이 F보다 뒤에 있는 사원만 조회
+
+
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.ENAME >= 'F'
+ORDER BY
+	e.ENAME ASC;
+
+-- JOB이 MANAGER 이거나, SALESMAN이거나, CLERK 사원 조회
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.JOB = 'MANAGER'
+	OR e.JOB = 'SALESMAN'
+	OR e.JOB = 'CLERK';
+
+-- -- JOB이 MANAGER 이거나, SALESMAN이거나, CLERK 사원 조회 (IN 사용)
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.JOB IN ('MANAGER', 'SALESMAN', 'CLERK');
+
+-- JOB이 (MANAGER, SALESMAN, CLERK)가 아닌 사원 조회 (IN 사용)
+SELECT
+	*
+FROM
+	EMP e
+WHERE
+	e.JOB NOT IN ('MANAGER', 'SALESMAN', 'CLERK');
+
+-- BETWEEN A AND B : A와 B 사이
+-- 급여가 2000~3000 사이인 사원을 조회
+SELECT * FROM EMP e WHERE e.SAL BETWEEN 2000 AND 3000;
+
+-- 급여가 2000~3000 사이가 아닌 사원 조회
+SELECT * FROM EMP e WHERE e.SAL NOT BETWEEN  2000 AND 3000;
+
+-- 검색(LIKE) 연산자와 와일드카드(_, %)
+-- _ : 어떤 값이든 상관없이 한 개의 문자열 데이터를 의미
+-- % : 길이와 상관없이(문자 없는 경우도 포함) 모든 문자열 데이터를 의미
+
+-- 사원명이 S로 시작하는 사원을 조회
+SELECT * FROM EMP e WHERE e.ename LIKE 'S%' ;
+
+-- 사원 이름의 두번째 글자가 L인 사원 조회
+SELECT * FROM EMP e WHERE e.ename LIKE '_L%' ;
+
+-- 사원명에 AM이 포함된 사원 조회
+SELECT * FROM EMP e WHERE e.ename LIKE '%AM%' ;
+-- 사원명에 AM이 포함되지 않은 사원 조회
+
+-- NULL 확인
+SELECT * FROM EMP e WHERE e.ename NOT LIKE '%AM%' ;
+
+-- MANAGER_ID가 NULL 인 사원 조회
+SELECT * FROM EMP e WHERE e.MGR IS NULL;
+
+-- MANAGER_ID가 NULL이 아닌 사원 조회
+SELECT * FROM EMP e WHERE e.MGR IS NOT NULL;
